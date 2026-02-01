07a2333eeae720379d389ac7e82436de2e287c44
Junnibun <allenkeimara@gmail.com>
2026-02-01 17:02:44 -0500

    Update config.js

--- config.js
+++ config.js
@@ -81,7 +81,7 @@ const CONFIG = {
         musicUrl: "https://clever-blush-kkyfd5nzru.edgeone.app/Clairo%20-%20Flamin%20Hot%20Cheetos%20(Lyrics).mp3", // Music streaming URL
         startText: "｡‧𝄞⨾𓍢ִ໋˚⋆.˚✮ʚ🎧ɞ✮˚.⋆𝄞⨾𓍢ִ໋ ",        // Button text to start music
         stopText: "STOP THE NOISE 🤕",         // Button text to stop music
-        volume: 0.5                        // Volume level (0.0 to 1.0)
+        volume: 1.0                        // Volume level (0.0 to 1.0)
     }
 };
 
