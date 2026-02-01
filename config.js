77439e1b94ee77c23774fc3707c607d16af555c1
Junnibun <allenkeimara@gmail.com>
2026-02-01 13:57:37 -0500

    Update config.js

--- config.js
+++ config.js
@@ -79,8 +79,8 @@ const CONFIG = {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
         musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
-        startText: "🎵 Play Music",        // Button text to start music
-        stopText: "🔇 Stop Music",         // Button text to stop music
+        startText: "⋆｡‧𝄞⨾𓍢ִ໋˚⋆.˚✮ʚ🎧ɞ✮˚.⋆𝄞⨾𓍢ִ໋",        // Button text to start music
+        stopText: "STOP THE NOISE 🤕",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
     }
 };
