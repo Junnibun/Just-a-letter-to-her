ecceda0785456975b39a879b6618ae9efe284684
Junnibun <allenkeimara@gmail.com>
2026-02-01 18:01:18 -0500

    Update config.js

--- config.js
+++ config.js
@@ -29,7 +29,6 @@ const CONFIG = {
         },
         second: {
             text: "How much do you love me?",                          // For the love meter
-                startText: "Sure...",                                   // Text before the percentage
             nextBtn: "Next ❤️"                                         // Text for the next button
         },
         third: {
